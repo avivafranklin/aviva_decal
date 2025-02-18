977f3ff854b5240e19fba2a7f210e38dc24da34c
avivafranklin <aviva_franklin@berkeley.edu>
2025-02-18 15:14:50 -0800

    adding completedhw2

---
+++ hw2.py
@@ -0,0 +1,33 @@
+# 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+#cd ..
+
+# 2) What does ~/ mean?
+#it represents the home directory of a user
+
+# 3) What's the difference between and abolute and a relative path in your terminal?
+# absolute tells you how to get there from your homr directory
+# relative tells you how to get there from where you are on your computer
+
+# 4) What command would bring you back to your home directory?
+# cd
+
+# 5) If you called rm ./ in your current working directory, what would it do? 
+#it would give an error message because rm by itself won't remove a directory only a file
+
+# 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
+# git add stages the changes to the file or directory that you are saving
+# git commit records the changes in the repository's history
+# git push uploads the changes to a remote repository
+
+# 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+# git status will tell you what changes you are making to your repository 
+
+# 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+# the most frustrating error is trying to add things to github with the wifi in Campbell not working. I ended up figuring out how to do it myself with better wifi.
+
+# 9) How does cloning a repository differ from pulling from a repository?
+# cloning a repository creates a whole new copy of the file/directory in your local repository
+# pulling a repository allows you to make changes to the already existing repository
+
+# 10) Tell me a fun fact!
+# i have both a dog and a cat!
